6d8242f96f119c3c3c366629cf49adb905a5480c
Cody Seibert <cseibert113@gmail.com>
2017-10-29 17:25:47 -0400

    added functionality for hovering over columns

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4 {
     this.COLS = 7;
     this.selector = selector;
     this.createGrid();
+    this.setupEventListeners();
   }
 
   createGrid() {
@@ -13,10 +14,37 @@ class Connect4 {
         .addClass('row');
       for (let col = 0; col < this.COLS; col++) {
         const $col = $('<div>')
-          .addClass('col empty');
+          .addClass('col empty')
+          .attr('data-col', col)
+          .attr('data-row', row);
         $row.append($col);
       }
       $board.append($row);
     }
+  }
+
+  setupEventListeners() {
+    const $board = $(this.selector);
+
+    function findLastEmptyCell(col) {
+      const cells = $(`.col[data-col='${col}']`);
+      for (let i = cells.length - 1; i >= 0; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass('empty')) {
+          return $cell;
+        }
+      }
+      return nulll;
+    }
+
+    $board.on('mouseenter', '.col.empty', function() {
+      const col = $(this).data('col');
+      const $lastEmptyCell = findLastEmptyCell(col);
+      $lastEmptyCell.addClass(`next-red`);
+    })
+
+    $board.on('mouseleave', '.col', function() {
+      $('.col').removeClass(`next-red`);
+    })
   }
 }
